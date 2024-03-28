c2237d6ee6b565730d4d320f863a9a79d1ca040f
Azhar Shaikh <azharshaikh33547@gmail.com>
2024-03-28 21:55:05 +0530

    initial files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'sans':["Noto Sans",'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
